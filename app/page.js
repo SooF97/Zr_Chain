be93988e4166167dbdd06f5b3f2c8ea9c377bd52
SooF97 <soufien.mosad97@hotmail.com>
2023-05-19 00:54:25 +0100

    third commit

--- app/page.js
+++ app/page.js
@@ -7,10 +7,10 @@ const Home = () => {
     <section className="w-full flex flex-col items-center px-4 md:px-8 lg:px-16 py-8">
       <h1 className=" orange_gradient head_text text-center text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
         Welcome to ZR Chain
-        {/* <br className="hidden md:inline" />
-        <span className="orange_gradient block mt-2 text-lg sm:text-xl md:text-2xl lg:text-3xl">
-          Legalization of documents in the blockchain
-        </span> */}
+        <br className="hidden md:inline" />
+        <span className="text-white block mt-2 text-lg sm:text-xl md:text-2xl lg:text-3xl">
+          Securing Documents & empowering trust using blockchain technology
+        </span>
       </h1>
       {/* <p className="desc text-center mt-4 text-sm sm:text-base md:text-lg lg:text-xl">
         This is a scientific project...
